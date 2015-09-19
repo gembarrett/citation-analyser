a065e373eb1099103dc4bd7dbc614974e71ca417
Gem Barrett <gem@gembarrett.com>
2015-09-19 15:43:59 -0400

    some styling added

--- main.js
+++ main.js
@@ -1,28 +1,43 @@
 $(document).ready(function(){
     $.getJSON('open-internet-footnotes.json', function(obj) {
       var noOfId = 0;
-      var testStr;
-      var prevStr;
-      var prevPrevStr;
+      var innerNoOfId = 0;
+      var innerInnerNoOfId = 0;
+      var innerInnerInnerNoOfId = 0;
          // for every object in the json
          for (var i = 0; i < obj.length; i++) {
            thisObj = obj[i];
-           // grab the text
-           testStr = thisObj.text;
            // if the text contains a reference to a previous citation
            if (checkForPreviousCitation(thisObj)){
              // add one to the tally
              noOfId++;
-             // print that shizzle out
-             console.log(thisObj.value + " - " + thisObj.text);
+             // prepare to check previous one
+             var prevObj = obj[i-1];
+             if (checkForPreviousCitation(prevObj)){
+               innerNoOfId++;
+               var prevPrevObj = obj[i-2];
+               if (checkForPreviousCitation(prevPrevObj)){
+                 innerInnerNoOfId++;
+                 var prevPrevPrevObj = obj[i-3];
+                 if (checkForPreviousCitation(prevPrevPrevObj)){
+                   innerInnerInnerNoOfId++;
+                 }
+               }
+             }
            }
         }
         $('h1 span').append(noOfId);
         console.log(noOfId);
+        $('h2 span').append(innerNoOfId);
+        console.log(innerNoOfId);
+        $('h3 span').append(innerInnerNoOfId);
+        console.log(innerInnerNoOfId);
+        $('h4 span').append(innerInnerInnerNoOfId);
+        console.log(innerInnerInnerNoOfId);
     });
 });
 
-function checkForPreviousCitation(thisObj, noOfId) {
+function checkForPreviousCitation(thisObj) {
   if (/(Id\.)|(Id\sat)/ig.test(thisObj.text)) {
     return true;
   }
