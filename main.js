37a5fd1abb34d9ffd3a0b0bf7e96ea7a8b6b808f
Gem Barrett <gem@gembarrett.com>
2015-09-19 15:05:40 -0400

    AWESOMELY BEAUTIFUL REGEX IS AWESOME I LOVE IT YAY

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
     $.getJSON('open-internet-footnotes.json', function(obj) {
-         alert(obj[0].text);
+      var noOfId = 0;
+      var testStr;
+      var prevStr;
+      var prevPrevStr;
+         // for every object in the json
+         for (var i = 0; i < obj.length; i++) {
+           // grab the text
+           testStr = obj[i].text ;
+           // if the text contains a reference to a previous citation
+           if (/(Id\.)|(Id\sat)/ig.test(testStr)) {
+             // add one to the tally
+             noOfId++;
+             console.log(obj[i].value + " - " + obj[i].text);
+            // grab the previous text
+            //  prevStr = obj[i -1].text;
+             // if the previous text contains a reference to a previous citation
+            //  if (/Id\.? at/i.test(prevStr)) {
+            //    prevPrevStr = obj[i-2].text;
+            //  } else {
+            //    console.log(prevPrevStr);
+            //  }
+           // if the text doesn't contain a reference to a previous citation
+           } else {
+            //  console.log(prevStr);
+           }
+        }
+        console.log(noOfId);
     });
 });
